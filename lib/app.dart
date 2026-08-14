91811ac4a4cd0cd362dd1a2fdf061cf8a1563660
Talha <talhahdev2025@gmail.com>
2026-08-14 14:51:17 +0500

    project setup

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuickBite extends StatelessWidget {
+  const QuickBite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text('this is quick bite app'))),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quick_bite/app.dart';
+
+void main() {
+  runApp(const QuickBite());
+}
