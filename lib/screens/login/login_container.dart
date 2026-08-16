b08f2b8a6b151ad86d33a05f069d97a3cc34e2c1
Talha <talhahdev2025@gmail.com>
2026-08-16 10:19:31 +0500

    chore : add textEditingControllers for all text fields in login screen

--- lib/screens/login/login_container.dart
+++ lib/screens/login/login_container.dart
@@ -5,17 +5,46 @@ import 'package:quick_bite/core/constants/app_text_styles.dart';
 import 'package:quick_bite/screens/login/authentication_container.dart';
 import 'package:quick_bite/screens/login/login_custom_text_field.dart';
 
-class LoginContainer extends StatelessWidget {
+class LoginContainer extends StatefulWidget {
   const LoginContainer({super.key});
+
+  @override
+  State<LoginContainer> createState() => _LoginContainerState();
+}
+
+class _LoginContainerState extends State<LoginContainer> {
+  late final TextEditingController _emailTextEditingController;
+  late final TextEditingController _passwordTextEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailTextEditingController = TextEditingController();
+    _passwordTextEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return AuthenticationContainer(
       btnText: 'Login',
       textFieldsList: [
-        LoginCustomTextField(labelText: 'Email address'),
+        LoginCustomTextField(
+          textEditingController: _emailTextEditingController,
+          labelText: 'Email address',
+        ),
         AppSpacing.vLg,
-        LoginCustomTextField(labelText: 'Password', obscureText: false),
+        LoginCustomTextField(
+          textEditingController: _passwordTextEditingController,
+          labelText: 'Password',
+          obscureText: false,
+        ),
         AppSpacing.vLg,
         Align(
           alignment: .centerStart,
--- lib/screens/login/sign_up_container.dart
+++ lib/screens/login/sign_up_container.dart
@@ -3,19 +3,46 @@ import 'package:quick_bite/core/constants/app_spacing.dart';
 import 'package:quick_bite/screens/login/authentication_container.dart';
 import 'package:quick_bite/screens/login/login_custom_text_field.dart';
 
-class SignUpContainer extends StatelessWidget {
+class SignUpContainer extends StatefulWidget {
   const SignUpContainer({super.key});
+
+  @override
+  State<SignUpContainer> createState() => _SignUpContainerState();
+}
+
+class _SignUpContainerState extends State<SignUpContainer> {
+  late final TextEditingController _userNameTextEditingController;
+  late final TextEditingController _passwordTextEditingController;
+  late final TextEditingController _emailTextEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _userNameTextEditingController = TextEditingController();
+    _emailTextEditingController = TextEditingController();
+    _passwordTextEditingController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return AuthenticationContainer(
       btnText: 'Sign Up',
       textFieldsList: [
-        LoginCustomTextField(labelText: 'User name'),
+        LoginCustomTextField(
+          textEditingController: _userNameTextEditingController,
+          labelText: 'User name',
+        ),
         AppSpacing.vLg,
-        LoginCustomTextField(labelText: 'Email address'),
+        LoginCustomTextField(
+          textEditingController: _emailTextEditingController,
+          labelText: 'Email address',
+        ),
         AppSpacing.vLg,
-        LoginCustomTextField(labelText: 'Password', obscureText: false),
+        LoginCustomTextField(
+          textEditingController: _passwordTextEditingController,
+          labelText: 'Password',
+          obscureText: false,
+        ),
       ],
     );
   }
